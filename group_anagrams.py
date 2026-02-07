c8519a137331885da9e87fc3edb75a68ac0c9eff
fenet-as <fenetasmamaw48@gmail.com>
2026-02-07 18:44:03 +0300

    Create group_anagrams.py

---
+++ group_anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            sorted_word = ''.join(sorted(list(word)))
+            anagrams[sorted_word].append(word)
+
+        res = []
+        for word in anagrams:
+            res.append(anagrams[word])
+
+        return res
