5dd7c8909a77ec2336583e40fc8b7feb431acf6a
fenet-as <fenetasmamaw48@gmail.com>
2026-02-24 12:01:23 +0300

    Time: 52 ms (44.32%), Space: 25.3 MB (45.71%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,42 +1,20 @@
 class Solution:
-    def numRescueBoats(self, nums: List[int], limit: int) -> int:
-        '''
-        people = [1,2], limit = 3
-                  i j
-
-        if n[i] + n[j] <= limit:
-            ct += 1
-            i += 1
-            j -= 1
-        elif n[i] + n[j] > limit:
-            j -= 1
-
-        [3,2,2,1]
-        [1,2,2,3] ct = 1
-           i   
-           j
-
-        '''
-        
-        nums.sort()
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
         i = 0
-        j = len(nums)-1
+        j = len(people)-1
 
-        ct = 0
-
+        count = 0
         while i < j:
-            sm = nums[i] + nums[j]
-            if sm <= limit:
-                ct += 1
-                i += 1
+            if people[i] + people[j] > limit:
                 j -= 1
             else:
+                i += 1
                 j -= 1
-                ct += 1
+            count += 1
+
+            
         if i == j:
-            ct += 1
+            count += 1
+        return count
 
-        return ct
-
-
-        
