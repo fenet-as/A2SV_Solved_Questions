c49c84013167e0c089808f6b80989cdc1de81946
fenet-as <fenetasmamaw48@gmail.com>
2026-03-03 15:58:46 +0300

    Create  Longest k-Good Segment time limit per test1 second.py

---
+++ Longest k-Good Segment time limit per test1 second.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline 
+
+n,k = map(int,(input().split()))
+
+arr = list(map(int,input().split()))
+
+i = 0
+mx = [0,0,0]
+st = {}
+
+for j in range(n):
+  if arr[j] not in st:
+    st[arr[j]] = 1
+  else:
+    st[arr[j]] += 1
+
+  while i < n and len(st) > k:
+    st[arr[i]] -= 1
+    if st[arr[i]] == 0:
+      del st[arr[i]]
+    i += 1
+
+  if mx[2] < j-i+1:
+    mx[2] = j-i+1
+    mx[0]=i+1
+    mx[1] = j+1
+
+
+print(mx[0],mx[1])
+
+
+
