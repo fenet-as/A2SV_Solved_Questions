6e9e322c069748d7009bab3b1b31d0c27c715946
fenet-as <fenetasmamaw48@gmail.com>
2026-03-23 19:20:18 +0300

    Create minPatches.py

---
+++ minPatches.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        
+
+        x = 0
+        patch_count = 0
+
+        i = 0
+
+        while x < n:
+            if i < len(nums) and nums[i] <= (x+1):
+                x += nums[i]
+                i += 1
+            else:
+                x += x+1
+                patch_count += 1
+
+        return patch_count
+
