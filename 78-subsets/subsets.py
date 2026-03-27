95a3d575d960e0764a276eea7d02a8b16fe35fd7
fenet-as <fenetasmamaw48@gmail.com>
2026-03-27 09:04:48 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (51.49%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,22 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
         res = []
         path = []
-        def helper(i):
-            if i >= len(nums):
+
+        def explore(i):
+            if i == len(nums):
                 res.append(path[:])
-                return 
-
-    
+                return
             res.append(path[:])
-
             for j in range(i,len(nums)):
-           
                 path.append(nums[j])
-                helper(j+1)
+                explore(j+1)
                 path.pop()
-
             return 
-        helper(0)
+
+        explore(0)
         return res
