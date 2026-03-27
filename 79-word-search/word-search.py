5c069aa4d493e63fcee247943c016d9d2ebc1290
fenet-as <fenetasmamaw48@gmail.com>
2026-03-27 14:35:21 +0300

    Time: 2 ms (98.20%) | Memory: 19.1 MB (99.09%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,50 +1,43 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
+        rows, cols = len(board), len(board[0])
 
-        
-        def search(i,j,m,row,col,word):
-            if m == len(word):
-                return True
-
-            if board[i][j] == "#":
-                return False
-            if board[i][j] == word[m]:
-                temp = board[i][j]
-                board[i][j] = "#"
-                m += 1
-                if m == len(word):
-                    return True
-
-                if i+1 < row:
-                    if search(i+1,j,m,row,col,word):
-                        return True
-                if i-1 >= 0:
-                    if search(i-1,j,m,row,col,word):
-                        return True
-                if j+1 < col:
-                    if search(i,j+1,m,row,col,word):
-                        return True
-                if j-1 >= 0:
-                    if search(i,j-1,m,row,col,word):
-                        return True
-                
-                board[i][j] = temp
+        # 🔹 Optimization 1: prune by character count
+        from collections import Counter
+        if Counter(word) - Counter(c for row in board for c in row):
             return False
 
-            
+        # 🔹 Optimization 2: reverse word if helpful
+        if word.count(word[0]) > word.count(word[-1]):
+            word = word[::-1]
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if search(i,j,0,len(board),len(board[0]),word):
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return False
+            
+            if board[r][c] != word[i]:
+                return False
+
+            # mark visited
+            temp = board[r][c]
+            board[r][c] = "#"
+
+            # explore 4 directions
+            for dr, dc in [(1,0), (-1,0), (0,1), (0,-1)]:
+                if dfs(r + dr, c + dc, i + 1):
+                    return True
+
+            # backtrack
+            board[r][c] = temp
+            return False
+
+        # try every starting cell
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
                     return True
 
         return False
-
-        
-            
-
-
-                
-
-                
