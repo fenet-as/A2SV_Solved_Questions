eadaf7dd4a19a8edcd576a30f542d05c3aab3aba
fenet-as <fenetasmamaw48@gmail.com>
2026-03-27 10:55:50 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (93.15%) - LeetSync

--- 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
+++ 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
@@ -7,8 +7,11 @@ class Solution:
             if i == len(s) and len(path) >= 2:
                 return True
 
+            curr = 0
             for j in range(i,len(s)):
-                curr = int(s[i:j+1])
+                curr = curr*10 + int(s[j])
+                if path and curr > path[-1]-1:
+                    break
                 if path and path[-1] - curr != 1:
                     continue
                 path.append(curr)
