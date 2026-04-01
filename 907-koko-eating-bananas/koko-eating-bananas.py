53728815dbd077bfe21dca20f5d987099596341e
fenet-as <fenetasmamaw48@gmail.com>
2026-04-01 17:31:25 +0300

    Time: 156 ms (89.52%) | Memory: 20.7 MB (62.95%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,45 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+
+        left = 1
+        right = max(piles)
+        ans = -1
+
+        # def possible(k):
+        #     t = 0
+
+        #     for e in piles:
+        #         if e <= k:
+        #             t += 1
+        #         else:
+        #             c = e
+        #             while c > k:
+        #                 c -= k
+        #                 t += 1
+        #             t += 1
+        #     print("t ",t)
+        #     if t > h:
+        #         return False
+        #     return True
+
+        def possible(k):
+            t = 0
+            for e in piles:
+                t += (e + k - 1) // k   # ceil division
+
+            return t <= h
+
+        while left <= right:
+            mid = left + (right-left)//2
+            # print(mid)
+
+            if possible(mid):
+                ans = mid
+                right = mid-1
+                # print("ans ",ans)
+            
+            else:
+                left = mid + 1 
+            
+        return ans 
