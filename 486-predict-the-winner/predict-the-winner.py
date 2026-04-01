2c4c377a2cbc95a1e6d67fef172087a2861e3644
fenet-as <fenetasmamaw48@gmail.com>
2026-04-01 22:15:39 +0300

    Time: 1 ms (67.72%) | Memory: 19.9 MB (31.96%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        from functools import lru_cache
+
+        @lru_cache(None)
+        def explore(i, j):
+            if i == j:
+                return nums[i]
+
+            pick_left = nums[i] - explore(i+1, j)
+            pick_right = nums[j] - explore(i, j-1)
+
+            return max(pick_left, pick_right)
+
+        return explore(0, len(nums)-1) >= 0
