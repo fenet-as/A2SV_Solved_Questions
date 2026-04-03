e9095c8370cdee1b834cda861ae4a085b6f197de
fenet-as <fenetasmamaw48@gmail.com>
2026-04-03 20:41:56 +0300

    Time: 578 ms (5.01%) | Memory: 20.1 MB (15.99%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        path = []
+        # st = defaultdict(int)
+        # ct = Counter(nums)
+        used = [False] * len(nums)
+        
+        def explore():
+            if len(path) == len(nums):
+                if path not in res:
+                    res.append(path[:])
+                return 
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                
+                path.append(nums[i])
+                # print(path)
+                used[i] = True
+                explore()
+                path.pop()
+                # print(path)
+                used[i] = False
+            return
+
+        explore()
+        return res
+
+            
+
+                
