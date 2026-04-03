46511926e10c8a4856fc3b77cdf5872c48f8c80a
fenet-as <fenetasmamaw48@gmail.com>
2026-04-04 01:00:09 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (72.65%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,22 +1,30 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        
-        res = []
+
+        res = [[]]
         path = []
         nums.sort()
-
+        
+        
         def explore(i):
             if i == len(nums):
-                res.append(path[:])
                 return
-            res.append(path[:])
+
+          
+
             for j in range(i,len(nums)):
                 if j > i and nums[j] == nums[j-1]:
                     continue
+
                 path.append(nums[j])
+                res.append(path[:])
                 explore(j+1)
                 path.pop()
-            return 
 
+            return 
         explore(0)
         return res
+
+
+
+            
