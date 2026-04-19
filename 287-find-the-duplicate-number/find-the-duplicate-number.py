8e9e721df8b58dacb223b415184ab2d45a427444
fenet-as <fenetasmamaw48@gmail.com>
2026-04-19 23:36:53 +0300

    Time: 25 ms (61.87%) | Memory: 33.6 MB (51.70%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,21 +1,19 @@
-from typing import List
-
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+
         slow = nums[0]
         fast = nums[0]
 
-        # Phase 1: find intersection point
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-            if slow == fast:
+
+            if fast == slow:
                 break
 
-        # Phase 2: find entrance to cycle
         slow = nums[0]
         while slow != fast:
-            slow = nums[slow]
             fast = nums[fast]
+            slow = nums[slow]
 
         return slow
