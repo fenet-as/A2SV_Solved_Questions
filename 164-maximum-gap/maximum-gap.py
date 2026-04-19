003cdd8130927ee76886861b80804295b3460380
fenet-as <fenetasmamaw48@gmail.com>
2026-04-19 20:05:55 +0300

    Time: 423 ms (11.30%) | Memory: 43 MB (5.36%) - LeetSync

--- 164-maximum-gap/maximum-gap.py
+++ 164-maximum-gap/maximum-gap.py
@@ -1,11 +1,32 @@
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        if len(nums) < 2:
             return 0
 
-        nums.sort()
-        mx = 0
-        for i in range(1,len(nums)):
-            mx = max(nums[i]-nums[i-1],mx)
+        mn,mx = min(nums), max(nums)
+        n = len(nums)
 
-        return mx
+        bs = max(1,(mx-mn)//(n-1))
+        bc = (mx-mn)//bs+1
+
+        bucket = [[float("inf"),float("-inf")]for _ in range(bc)]
+
+
+        for e in nums:
+            ind = (e-mn)//bs
+            
+            bucket[ind][0] = min(e,bucket[ind][0])
+            bucket[ind][1] = max(e,bucket[ind][1])
+
+        pmax = mn
+        d = 0
+
+        for bmin,bmax in bucket:
+            if bmin == float("inf"):
+                continue
+            d = max(d,bmin - pmax)
+            pmax = bmax
+        return d
+
+            
+
