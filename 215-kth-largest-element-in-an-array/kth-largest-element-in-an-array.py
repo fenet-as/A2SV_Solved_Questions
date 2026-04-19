008613a889b7355301778b9a575d8fbfac6c4fc3
fenet-as <fenetasmamaw48@gmail.com>
2026-04-19 23:09:34 +0300

    Time: 102 ms (19.25%) | Memory: 31.1 MB (35.73%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,33 +1,31 @@
-import random
-from typing import List
-
 class Solution:
-    def findKthLargest(self, arr: List[int], k: int) -> int:
-        target = len(arr) - k
-        left, right = 0, len(arr) - 1
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums)-k
+        left, right = 0,len(nums)-1
+
 
         while True:
-            pivot = arr[random.randint(left, right)]
+            p = random.randint(left, right)
+            lw,rw,r = left,right,left
+            pivot = nums[p]
 
-            # 3-way partition
-            l, m, r = left, left, right
-
-            while m <= r:
-                if arr[m] < pivot:
-                    arr[l], arr[m] = arr[m], arr[l]
-                    l += 1
-                    m += 1
-                elif arr[m] > pivot:
-                    arr[m], arr[r] = arr[r], arr[m]
-                    r -= 1
+            while r <= rw:
+                if nums[r] < pivot:
+                    nums[lw],nums[r] = nums[r], nums[lw]
+                    lw += 1
+                    r += 1
+                
+                elif  nums[r] > pivot: 
+                    nums[rw],nums[r] = nums[r],nums[rw]
+                    rw -= 1
                 else:
-                    m += 1
-
-        
-
-            if target < l:
-                right = l - 1
-            elif target > r:
-                left = r + 1
+                    r += 1
+            
+            if k < lw:
+                right = lw-1
+            elif k > rw:
+                left = rw + 1
             else:
                 return pivot
+
+
