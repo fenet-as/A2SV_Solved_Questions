deff460333fa83c7f81a927272f3d08209a7ea68
fenet-as <fenetasmamaw48@gmail.com>
2026-04-23 20:42:22 +0300

    Time: 275 ms (15.19%) | Memory: 31.4 MB (5.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,27 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        direct = [[0,1] , [1,0], [0,-1] , [-1,0]]
-        visited = set()
-        res = 0
-        def inbound(r,c):
-            return 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == "1"
-
-        def dfs(row , col):
-            visited.add((row,col))
-            
-            for x , y in direct:
-                newr , newc = row + x , col + y
-                if inbound(newr,newc) and  (newr,newc) not in visited :
-                    dfs(newr,newc)
-                    
-                   
         
+        visited = set()
 
+        dir = [(0,1),(1,0),(-1,0),(0,-1)]
+
+        def in_bound(i,j):
+            return 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == "1"
+
+        def dfs(i,j):
+
+            visited.add((i,j))
+
+            for x,y in dir:
+                nr,nc = i + x,j + y
+
+                if in_bound(nr,nc) and (nr,nc) not in visited:
+                    dfs(nr,nc)
+
+        islands = 0    
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1" and (i,j) not in visited:
-                    dfs(i , j)
-                    res += 1
-        return res
-
-        
+                    dfs(i,j)
+                    islands += 1
+        return islands
+                
+                
