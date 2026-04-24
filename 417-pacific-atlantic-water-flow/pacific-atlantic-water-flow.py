403a102d0dead4fd0aff5d9913f7baa581f12a55
fenet-as <fenetasmamaw48@gmail.com>
2026-04-24 19:20:09 +0300

    Time: 87 ms (9.17%) | Memory: 39.2 MB (5.04%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,30 +1,64 @@
 class Solution:
-    def pacificAtlantic(self, heights):
-        m, n = len(heights), len(heights[0])
-        dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        
 
-        def bfs(starts):
-            visited = set()
-            stack = list(starts)
+        dir = [[0,1],[1,0],[-1,0],[0,-1]]
 
-            while stack:
-                r, c = stack.pop()
-                if (r, c) in visited:
-                    continue
-                visited.add((r, c))
 
-                for dr, dc in dirs:
-                    nr, nc = r + dr, c + dc
-                    if (0 <= nr < m and 0 <= nc < n and
-                        heights[nr][nc] >= heights[r][c]):
-                        stack.append((nr, nc))
 
-            return visited
+        def inbound(i,j):
+            return 0 <= i < len(heights) and 0 <= j < len(heights[0])
 
-        pacific_starts = [(0,j) for j in range(n)] + [(i,0) for i in range(m)]
-        atlantic_starts = [(m-1,j) for j in range(n)] + [(i,n-1) for i in range(m)]
+        state = {(i,j):[0,0] for i in range(len(heights)) for j in range(len(heights[0]))}
 
-        pac = bfs(pacific_starts)
-        atl = bfs(atlantic_starts)
+        visited = [set(),set()]
 
-        return list(pac & atl)
+
+        def dfs(i,j,t):
+
+            if (i,j) in visited[t]:
+                return
+            visited[t].add((i,j))
+            state[(i,j)][t] = 1                
+
+            for x,y in dir:
+                nr,nc = i+x, j+y
+
+                if inbound(nr,nc) and (nr,nc) not in visited[t]:
+                    if heights[nr][nc] >= heights[i][j]:
+                        dfs(nr,nc,t)
+                
+                
+
+            
+        # left - pac
+        for i in range(len(heights)):
+            dfs(i,0,1)
+        
+
+        # right - atl
+        for i in range(len(heights)):
+            dfs(i,len(heights[0])-1,0)
+        
+
+        # top - pac
+        for j in range(len(heights[0])):
+            dfs(0,j,1)
+        # buttom - atla
+        for j in range(len(heights[0])):
+            dfs(len(heights)-1,j,0)            
+                
+
+        res = [list(e) for e in state if state[e] == [1,1] ]
+
+        return res
+
+
+         
+                  
+
+                
+
+
+
+
