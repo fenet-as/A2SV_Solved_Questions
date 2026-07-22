433b3915eb6c1312353fee28b1c67a9e5904788c
fenet-as <fenetasmamaw48@gmail.com>
2026-07-22 10:59:34 +0300

    Time: 11 ms (53.66%) | Memory: 19.6 MB (88.87%) - LeetSync

--- 482-license-key-formatting/license-key-formatting.py
+++ 482-license-key-formatting/license-key-formatting.py
@@ -1,22 +1,15 @@
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        ans = []
+        ans = ''
         count = 0
 
-        res = []
         for c in s[::-1]:
             if c != "-":
                 count += 1
-                res.append(c.upper())
+                ans += c.upper()
             if count == k:
                 count = 0
-                ans.append(''.join(res))
-                ans.append("-")
-                res = []
-        ans.append(''.join(res))
-     
-        ans = ''.join(ans)
-        
+                ans += "-"
 
         if len(ans) != 0 and ans[-1] == "-":
             ans = ans[:-1]
